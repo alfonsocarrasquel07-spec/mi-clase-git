d25f7fccf236bb925efdfc7de0befa2953b96690
Richard Alfonso Carrasquel <alfonsocarrasquel07@gmail.com>
2026-04-14 15:16:33 -0400

    Agregado mi taller de C# desde SharpDevelop

---
+++ Tallerr/Tallerr/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: User
+ * Fecha: 14/04/2026
+ * Hora: 15:14
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Tallerr
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
